574c8bb3171e9bb2746d2b86c01f05f0231fe1ed
Zhaoting Chen <ztchenustc@gmail.com>
2025-08-18 01:44:38 +0100

    feat: rewrite to allow parameter change

--- src/py21cmmclite/coeval.py
+++ src/py21cmmclite/coeval.py
@@ -2,9 +2,29 @@ import logging
 import numpy as np
 import py21cmfast as p21
 from py21cmfast.io.caching import OutputCache
-from ._util import BaseSimulator
 
 logger = logging.getLogger("21cmFAST")
+
+
+class BaseSimulator:
+    """
+    Base class for constructing simulators and likelihoods.
+    """
+
+    def __init__(
+        self,
+        inputs_21cmfast: p21.InputParameters,
+        cache_dir: str,
+        regenerate: bool = False,
+        global_params: dict | None = None,
+    ):
+        self.inputs_21cmfast = inputs_21cmfast
+        self.cache_dir = cache_dir
+        self.regenerate = regenerate
+        self.global_params = global_params or {}
+
+    def get_update_input(self, update_dict: dict):
+        return self.inputs_21cmfast.evolve_input_structs(**update_dict)
 
 
 class CoevalSimulator(BaseSimulator):
@@ -16,33 +36,30 @@ class CoevalSimulator(BaseSimulator):
         self,
         redshifts: list[float] | np.ndarray,
         inputs_21cmfast: p21.InputParameters,
-        varied_params: dict,
         cache_dir: str,
         regenerate: bool = False,
         global_params: dict | None = None,
     ):
-        super().__init__(
-            inputs_21cmfast, varied_params, cache_dir, regenerate, global_params
-        )
+        super().__init__(inputs_21cmfast, cache_dir, regenerate, global_params)
         self.redshifts = redshifts
 
-    def simulate(self):
+    def simulate(self, update_params: dict = {}):
         """
         Simulate the coeval cubes.
         """
+        inputs = self.get_update_input(update_params)
         cache = OutputCache(self.cache_dir)
-        coeval = p21.run_coeval(
+        p21.run_coeval(
             out_redshifts=self.redshifts,
-            inputs=self.inputs_simulator,
+            inputs=inputs,
             write=True,
             regenerate=self.regenerate,
             cache=cache,
             **self.global_params,
         )
-        # coeval is reshuffled in the order of redshift, so we need to get the redshift of each coeval
         return 1.0
 
-    def build_model_data(self):
+    def build_model_data(self, update_params: dict = {}):
         pass
 
 
@@ -51,12 +68,9 @@ class CoevalNeutralFraction(CoevalSimulator):
     Simulate the coeval cubes and calculate the 1DPS.
     """
 
-    def build_model_data(self):
-        super().build_model_data()
-        xhibox = [
-            p21.IonizedBox.new(redshift=z, inputs=self.inputs_simulator)
-            for z in self.redshifts
-        ]
+    def build_model_data(self, update_params: dict = {}):
+        inputs = self.get_update_input(update_params)
+        xhibox = [p21.IonizedBox.new(redshift=z, inputs=inputs) for z in self.redshifts]
         cache = OutputCache(self.cache_dir)
         xhibox = [
             p21.io.h5.read_output_struct(cache.find_existing(path)) for path in xhibox
--- src/py21cmmclite/likelihood.py
+++ src/py21cmmclite/likelihood.py
@@ -1,5 +1,4 @@
-from .coeval import CoevalNeutralFraction
-from ._util import BaseSimulator
+from .coeval import CoevalNeutralFraction, BaseSimulator
 import numpy as np
 import py21cmfast as p21
 
@@ -18,43 +17,123 @@ class LikelihoodBase(BaseSimulator):
     def __init__(
         self,
         inputs_21cmfast: p21.InputParameters,
-        varied_params: dict,
+        varied_params: list[str],
         cache_dir: str,
         regenerate: bool = False,
         global_params: dict | None = None,
+        data_dict: dict | None = None,
+    ):
+        super().__init__(inputs_21cmfast, cache_dir, regenerate, global_params)
+        self.varied_params = varied_params
+        self.simulators = []
+        self.data_dict = data_dict
+
+    def get_update_dict(self, varied_params_values):
+        if not len(varied_params_values) == len(self.varied_params):
+            raise ValueError("input values must have the same length as varied_params")
+        update_params = {
+            self.varied_params[i]: varied_params_values[i]
+            for i in range(len(self.varied_params))
+        }
+        return update_params
+
+    def invoke_simulators(self, params_values=None):
+        if params_values is None:
+            update_dict = {}
+        else:
+            assert len(params_values) == len(
+                self.varied_params
+            ), "input values must have the same length as varied_params"
+            update_dict = self.get_update_dict(params_values)
+        for simulator in self.simulators:
+            simulator.simulate(update_dict)
+
+    def gather_model(self, params_values=None):
+        self.invoke_simulators(params_values)
+        if params_values is None:
+            update_dict = {}
+        else:
+            update_dict = self.get_update_dict(params_values)
+        model_vector = []
+        for simulator in self.simulators:
+            model_vector.append(simulator.build_model_data(update_dict))
+        return model_vector
+
+    def gather_data(self):
+        pass
+
+    def likelihood_function(self, model, data):
+        pass
+
+    def compute_likelihood(self, varied_params_values):
+        return self.likelihood_function(
+            self.gather_model(varied_params_values), self.gather_data()
+        )
+
+
+class LikelihoodGaussian(LikelihoodBase):
+    """
+    Class for Gaussian likelihoods.
+    """
+
+    def __init__(
+        self,
+        inputs_21cmfast: p21.InputParameters,
+        varied_params: list[str],
+        cache_dir: str,
+        regenerate: bool = False,
+        global_params: dict | None = None,
+        data_dict: dict | None = None,
+        simulate_data: bool = False,
+        simulate_error_fraction: float = 0.1,
     ):
         super().__init__(
             inputs_21cmfast, varied_params, cache_dir, regenerate, global_params
         )
-        self.simulators = []
-        self._model_vector = []
-        self._simulated = False
+        if data_dict is None:
+            data_vector = None
+            data_inv_covariance = None
+        else:
+            data_vector = data_dict["data_vector"]
+            data_inv_covariance = data_dict["data_inv_covariance"]
+        if data_inv_covariance is not None:
+            if data_inv_covariance.shape == data_vector.shape:
+                self.data_inv_covariance = np.diag(data_inv_covariance.ravel())
+            elif data_inv_covariance.shape == (data_vector.size, data_vector.size):
+                self.data_inv_covariance = data_inv_covariance
+            else:
+                raise ValueError(
+                    "data_inv_covariance must be a square matrix with the same size as data_vector,"
+                    "or a vector with the same size as data_vector assuming diagonal covariance"
+                )
+        self.data_dict = {
+            "data_vector": data_vector,
+            "data_inv_covariance": data_inv_covariance,
+        }
+        self.simulate_data = simulate_data
+        self.simulate_error_fraction = simulate_error_fraction
 
-    def invoke_simulators(self):
-        for simulator in self.simulators:
-            simulator.simulate()
-        self._simulated = True
+    def gather_data(self):
+        if self.simulate_data:
+            # use default params to simulate data
+            data_vec = np.array(self.gather_model()).ravel()
+            inv_cov = 1 / (np.diag(data_vec) * self.simulate_error_fraction) ** 2
+            return {
+                "data_vector": data_vec,
+                "data_inv_covariance": inv_cov,
+            }
+        else:
+            return self.data_dict
 
-    def gather_model_vector(self):
-        if not self._simulated:
-            raise ValueError(
-                "Simulators have not been invoked yet. "
-                "Please run invoke_simulators() first."
-            )
-        model_vector = []
-        for simulator in self.simulators:
-            model_vector.append(simulator.build_model_data())
-        self._model_vector = model_vector
-        return model_vector
-
-    def gather_data_vector(self):
-        pass
-
-    def compute_likelihood(self):
-        pass
+    def likelihood_function(self, model, data):
+        model_vector = np.array(model).ravel()
+        data_vector = data["data_vector"]
+        data_inv_covariance = data["data_inv_covariance"]
+        chi2 = compute_chi2(model_vector, data_vector, data_inv_covariance)
+        return -0.5 * chi2
 
 
-class LikelihoodNeutralFraction(LikelihoodBase):
+class LikelihoodNeutralFraction(LikelihoodGaussian):
     """
     Likelihood for neutral fraction.
     """
@@ -63,36 +142,31 @@ class LikelihoodNeutralFraction(LikelihoodBase):
         self,
         redshifts: list[float] | np.ndarray,
         inputs_21cmfast: p21.InputParameters,
-        varied_params: dict,
+        varied_params: list[str],
         cache_dir: str,
-        data_vector: np.ndarray,
-        data_inv_covariance: np.ndarray,
         regenerate: bool = False,
         global_params: dict | None = None,
+        data_dict: dict | None = None,
+        simulate_data: bool = False,
+        simulate_error_fraction: float = 0.1,
     ):
         super().__init__(
-            inputs_21cmfast, varied_params, cache_dir, regenerate, global_params
+            inputs_21cmfast,
+            varied_params,
+            cache_dir,
+            regenerate,
+            global_params,
+            data_dict,
+            simulate_data,
+            simulate_error_fraction,
         )
         self.redshifts = redshifts
         self.simulators = [
             CoevalNeutralFraction(
                 redshifts=redshifts,
                 inputs_21cmfast=inputs_21cmfast,
-                varied_params=varied_params,
                 cache_dir=cache_dir,
                 regenerate=regenerate,
                 global_params=global_params,
             )
         ]
-        self.data_vector = data_vector
-        if np.allclose(data_inv_covariance.shape, (data_vector.size, data_vector.size)):
-            self.data_inv_covariance = data_inv_covariance
-        else:
-            raise ValueError(
-                "data_inv_covariance must be a square matrix with the same size as data_vector"
-            )
-
-    def compute_likelihood(self):
-        model_vector = self._model_vector
-        chi2 = compute_chi2(model_vector, self.data_vector, self.data_inv_covariance)
-        return -0.5 * chi2
