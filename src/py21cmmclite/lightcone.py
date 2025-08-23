c01434b397629a63902ead0374441fc31b5687ef
Zhaoting Chen <ztchenustc@gmail.com>
2025-08-23 03:19:14 +0100

    feat: add lya forest simulator

--- src/py21cmmclite/lightcone.py
+++ src/py21cmmclite/lightcone.py
@@ -31,6 +31,53 @@ def get_lc_file_path(cache_dir: str, inputs: p21.InputParameters):
     return file_path
 
 
+def tau_GP(cosmo_params, gamma_bg, delta, temp, redshifts):
+    r"""Calculating the lyman-alpha optical depth in each pixel using the fluctuating GP approximation.
+    Parameters
+    ----------
+    gamma_bg : float or array_like
+        The background photonionization rate in units of 1e-12 s**-1
+    delta : float or array_like
+        The underlying overdensity
+    temp : float or array_like
+        The kinectic temperature of the gas in 1e4 K
+    redshifts : float or array_like
+        Correspoding redshifts along the los
+    """
+    gamma_local = np.zeros_like(gamma_bg)
+    residual_xHI = np.zeros_like(gamma_bg, dtype=np.float64)
+    flag_neutral = gamma_bg == 0
+    flag_zerodelta = delta == 0
+    if gamma_bg.shape != redshifts.shape:
+        redshifts = np.tile(redshifts, (*gamma_bg.shape[:-1], 1))
+    delta_ss = 2.67e4 * temp**0.17 * (1.0 + redshifts) ** -3 * gamma_bg ** (2.0 / 3.0)
+    gamma_local[~flag_neutral] = gamma_bg[~flag_neutral] * (
+        0.98
+        * ((1.0 + (delta[~flag_neutral] / delta_ss[~flag_neutral]) ** 1.64) ** -2.28)
+        + 0.02 * (1.0 + (delta[~flag_neutral] / delta_ss[~flag_neutral])) ** -0.84
+    )
+    Y_He = 0.245
+    # TODO: use global_params
+    residual_xHI[~flag_zerodelta] = 1 + gamma_local[~flag_zerodelta] * 1.0155e7 / (
+        1.0 + 1.0 / (4.0 / Y_He - 3)
+    ) * temp[~flag_zerodelta] ** 0.75 / (
+        delta[~flag_zerodelta] * (1.0 + redshifts[~flag_zerodelta]) ** 3
+    )
+    residual_xHI[~flag_zerodelta] = residual_xHI[~flag_zerodelta] - np.sqrt(
+        residual_xHI[~flag_zerodelta] ** 2 - 1.0
+    )
+    return (
+        7875.053145028655
+        / (
+            cosmo_params.hlittle
+            * np.sqrt(cosmo_params.OMm * (1.0 + redshifts) ** 3 + cosmo_params.OMl)
+        )
+        * delta
+        * (1.0 + redshifts) ** 3
+        * residual_xHI
+    )
+
+
 class LightconeSimulator(EoRSimulator):
     """
     Base class for constructing simulators of lightcone using 21cmFAST.
@@ -227,3 +274,153 @@ class LightconeNeutralFraction(LightconeSimulator):
         if self.save_xhi_lc:
             blob["xhi_lc"] = np.array(xhi_lc)
         return xhi_points, blob
+
+
+class LightconeLyaOpticalDepth(LightconeSimulator):
+    """
+    Simulate the lightcone and calculate the lya optical depth.
+    """
+
+    def __init__(
+        self,
+        redshift_bin_edges: list[float],
+        inputs: p21.InputParameters,
+        cache_dir: str,
+        regenerate: bool = False,
+        global_params: dict | None = None,
+        lc_min_redshift: float | None = None,
+        lc_max_redshift: float | None = None,
+        lc_quantities: list[str] = [
+            "brightness_temp",
+            "neutral_fraction",
+            "ionisation_rate_G12",
+            "density",
+            "kinetic_temperature",
+        ],
+        correct_gp_to_hydro: bool = False,
+        correct_gp_to_hydro_mapping: str | None = None,
+        max_correct_filling_factor: float = 0.7,
+        kde_repeat_num: int = 30,
+        inverse_tau_bin_edges: list[float] = np.linspace(0 - 0.0025, 1 + 0.0025, 202),
+        save_tau_gp: bool = False,
+        save_inv_tau_pdf: bool = False,
+        model_err_fraction: float = 0.0,
+    ):
+        super().__init__(
+            inputs,
+            cache_dir,
+            regenerate,
+            global_params,
+            lc_min_redshift,
+            lc_max_redshift,
+            lc_quantities,
+        )
+        self.redshift_bin_edges = redshift_bin_edges
+        self.save_tau_gp = save_tau_gp
+        self.save_inv_tau_pdf = save_inv_tau_pdf
+        self.correct_gp_to_hydro = correct_gp_to_hydro
+        self.correct_gp_to_hydro_mapping = correct_gp_to_hydro_mapping
+        if self.correct_gp_to_hydro:
+            self.correct_gp_to_hydro_mapping = os.path.join(
+                os.path.dirname(__file__), "data/Forests/tau_mapping.npy"
+            )
+            self.cpdf = np.load(
+                self.correct_gp_to_hydro_mapping, allow_pickle=True
+            ).item()
+        self.max_correct_filling_factor = max_correct_filling_factor
+        self.kde_repeat_num = kde_repeat_num
+        self.inverse_tau_bin_edges = inverse_tau_bin_edges
+        self.model_err_fraction = model_err_fraction
+
+    @property
+    def redshift_bin_centers(self):
+        return (self.redshift_bin_edges[:-1] + self.redshift_bin_edges[1:]) / 2
+
+    def build_model_data(self, update_params: dict = {}):
+        inputs = self.get_update_input(update_params)
+        lc_file_path = get_lc_file_path(self.cache_dir, inputs)
+        if not os.path.exists(lc_file_path):
+            raise FileNotFoundError(f"Lightcone file not found: {lc_file_path}")
+        with h5py.File(lc_file_path, "r") as f:
+            z_lc = np.array(f["lightcone_redshifts"])
+            z_sel_list = [
+                np.logical_and(
+                    z_lc >= self.redshift_bin_edges[i],
+                    z_lc < self.redshift_bin_edges[i + 1],
+                )
+                for i in range(len(self.redshift_bin_edges) - 1)
+            ]
+            tau_gp = [
+                tau_GP(
+                    self.inputs.cosmo_params,
+                    np.array(f["lightcones"]["ionisation_rate_G12"])[:, :, z_sel],
+                    1 + np.array(f["lightcones"]["density"])[:, :, z_sel],
+                    np.array(f["lightcones"]["kinetic_temperature"])[:, :, z_sel] / 1e4,
+                    z_lc[z_sel],
+                )
+                for z_sel in z_sel_list
+            ]
+            tau_gp = [
+                -np.log(np.mean(np.exp(-tau_i), axis=-1)).ravel() for tau_i in tau_gp
+            ]
+            tau_gp = np.array(tau_gp)
+            n_los = self.inputs.simulation_options.cell_size**2
+            if self.correct_gp_to_hydro:
+                tau_hydros = []
+                filling_factor = [
+                    np.array(f["lightcones"]["neutral_fraction"])[:, :, z_sel].mean()
+                    for z_sel in z_sel_list
+                ]
+                for i in range(len(self.redshift_bin_edges) - 1):
+                    if filling_factor[i] < self.max_correct_filling_factor:
+                        tau_hydros_i = (
+                            self.cpdf.sample(
+                                inherent_conditionals={
+                                    "z": self.redshift_bin_centers[i],
+                                    "xHI": filling_factor[i],
+                                },
+                                conditionals={
+                                    "inversetau_GP": np.array(
+                                        [
+                                            tau_gp[i] ** -1,
+                                        ]
+                                        * self.kde_repeat_num
+                                    ).ravel()
+                                },
+                                n_samples=1,
+                                random_state=None,  # different realizations
+                                keep_dims=False,
+                            ).flatten()
+                            ** -1
+                        )
+                        tau_hydros.append(tau_hydros_i)
+                    else:
+                        tau_hydros.append(
+                            np.array(
+                                [
+                                    tau_gp[i] ** -1,
+                                ]
+                                * self.kde_repeat_num
+                            ).ravel()
+                        )
+                tau_hydros = np.array(tau_hydros)
+                tau_gp = tau_hydros
+        tau_pdf = []
+        for i in range(len(self.redshift_bin_edges) - 1):
+            tau_gp_i = tau_gp[i]
+            if self.model_err_fraction > 0:
+                tau_gp_i = tau_gp_i * (
+                    1 + np.random.normal(0, self.model_err_fraction, tau_gp[i].shape)
+                )
+                tau_gp_i[tau_gp_i < 0] = 0
+            tau_pdf_i = np.histogram(
+                tau_gp_i**-1, bins=self.inverse_tau_bin_edges, density=True
+            )[0]
+            tau_pdf.append(tau_pdf_i)
+        tau_pdf = np.array(tau_pdf)
+        blob = {}
+        if self.save_tau_gp:
+            blob["tau_forest_gp"] = tau_gp
+        if self.save_inv_tau_pdf:
+            blob["inv_tau_forest_pdf"] = tau_pdf
+        return tau_pdf, blob
